72bf388b713776f85f602c4f2167ab70d636ab51
YashBhalekar09 <ybhalekar09@gmail.com>
2026-08-20 18:16:06 +0530

    Connecting frontend

--- src/main/java/com/websocket/WebsocketProject/config/WebSocketConfig.java
+++ src/main/java/com/websocket/WebsocketProject/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("https://websocket-fe-g6p2.onrender.com");
     }
 
     @Override
